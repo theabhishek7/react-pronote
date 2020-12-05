d7b5965378935fd047f4867b8bfe36d06eb22840
[Kumar Abhishek] <[krabhi745@gmail.com]>
2020-12-05 11:41:37 +0530

    first commit

---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import './Editor.css'
+export default class Editor extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+    render() {
+        return ( <
+            div >
+            <
+            div className = "container" >
+
+            <
+            div className = "input" >
+            <
+            h3 > Input < /h3> <
+            textarea className = "input-text"
+            onChange = { this.handleChange }
+            defaultValue = { this.state.value }
+            /> < /
+            div > <
+            div className = "output" >
+            <
+            h3 > Pro Note < /h3> <
+            div className = "output-text" > { this.state.value } < /div> < /
+            div > <
+            /div>
+
+            <
+            /div>
+        );
+    }
+}
